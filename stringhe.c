f2f009c7d5426b78c6fd829b218002f43169e8ef
nicolo <nicolo.bertoncello@iisviolamarchesini.edu.it>
2022-04-08 08:50:12 -0700

    secondo  metodo

--- stringhe.c
+++ stringhe.c
@@ -1,10 +1,7 @@
 #include<stdio.h>  
 #include <string.h>    
 int main(){    
-char name[50];    
-printf("Enter your name: ");    
-gets(name); //reads string from user    
-printf("Your name is: ");    
-puts(name);  //displays string    
-return 0;    
+char stringa[20]={'c', 'i', 'a', 'o', '\0'};    
+   printf("contenuto stringa: %d",strlen(stringa));    
+ return 0;    
 }    
