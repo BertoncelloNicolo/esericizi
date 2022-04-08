236ec4da780893a799bc2ddf838e540fb6ccf1c7
nicolo <nicolo.bertoncello@iisviolamarchesini.edu.it>
2022-04-08 08:52:58 -0700

    terzo metodo

--- stringhe.c
+++ stringhe.c
@@ -1,7 +1,9 @@
 #include<stdio.h>  
 #include <string.h>    
 int main(){    
-char stringa[20]={'c', 'i', 'a', 'o', '\0'};    
-   printf("contenuto stringa: %d",strlen(stringa));    
+ char stringa[5]={'t', 'p', 's', 'i', 't', '\0'};    
+   char stringa2[12];    
+   strcpy(stringa2,ch);    
+   printf("Value of second string is: %s",stringa2);    
  return 0;    
 }    
