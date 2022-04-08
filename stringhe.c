831c54662979b5770ff184a11380002c04c4175d
nicolo <nicolo.bertoncello@iisviolamarchesini.edu.it>
2022-04-08 08:54:35 -0700

    quarto metodo

--- stringhe.c
+++ stringhe.c
@@ -1,9 +1,9 @@
 #include<stdio.h>  
 #include <string.h>    
 int main(){    
- char stringa[5]={'t', 'p', 's', 'i', 't', '\0'};    
-   char stringa2[12];    
-   strcpy(stringa2,ch);    
-   printf("Value of second string is: %s",stringa2);    
+  char stringa1[8]={'t', 'p', 's', 'i', 't', '\0'};    
+   char stringa2[10]={'m', '\0'};    
+   strcat(stringa1,stringa2);    
+   printf("Value of first string is: %s",stringa1);    
  return 0;    
 }    
